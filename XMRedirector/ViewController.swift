620ec91ce8a5ac0491945457f15fc98f2817cb54
Michael Teeuw <michael@xonaymedia.nl>
2014-09-11 20:07:17 +0200

    Initial Commit

---
+++ XMRedirector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XMRedirector
+//
+//  Created by Michael Teeuw on 11-09-14.
+//  Copyright (c) 2014 Michael Teeuw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
